1af1c963d571bae9f15f370d7e543145b59ba049
Kasia <kasi.skowronska@gmail.com>
2018-01-14 19:34:08 +0100

    data in dict, saving data in right format

--- src/links_spider.py
+++ src/links_spider.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from collections import OrderedDict
 
 from  glomerulus.config import LINKS_PATH
 from  glomerulus.io import FileStorage
@@ -24,64 +25,105 @@ class LinksSpider(scrapy.Spider):
                 requests.append(scrapy.Request(url))
         return requests
 
+
+
     def parse(self, response):
-        # Extract structured data and return a dictionary of values to save/use
-        #print("TUTAJ: " + response.css('title::text').extract_first())
-        #return [{'xyz': 'sdff'}]
-        self.get_content(response)
+
+        article_content, article_comments = self.get_content(response)
+
+        data = OrderedDict([
+            ('url',response.url),
+            ('is_fake',"TODO"),
+            ('publish_date', "TODO"),
+            ('content', article_content),
+            ('comments', article_comments)]
+        )
+        self.save_article_data(data)
+
+
+    def save_article_data(self, data):
+        # Should be somewhere else? TODO better?
+        str = ""
+        for key, val in data.items():
+            str += (key + ": " + val)
+            str += "\n"
+        str += "\n"
+        FileStorage().save_data('first-data', str, flag='a')
 
 
     def get_content(self, response):
 
-        '''
-
-        possible upgrades: get content  from e.i. <p> <strong> blabla </strong><p>
-        '''
-
         # Indentify main container
         article = self.extract_main_container(response)
 
-        # We get all p inside main_container
-        all_pars = article.xpath('.//p/text()').extract()
+        # Get all p inside main container
+        all_pars = self.extract_main_content(article)
 
-        # Extract all p likely to be comments inside main_container
+        # Extract all p likely to be comments inside main container
         comment_pars = self.extract_comments_inside_main(article)
 
-        # Remove comments from main_container
+        # Remove comments from main content
         content_pars = self.remove_elements(all_pars, comment_pars)
 
-        #Generate data to write
-        content = ""
-        content += "\n CAŁY ARTYKUŁ --- \n"
-        for p in all_pars:
-            content += p
-        content += "\n KOMENTARZE --- \n"
-        for p in comment_pars:
-            content += p
-        content += "\n KONTENT --- \n"
-        for par in content_pars:
-            content+=par
 
-        FileStorage().save_data('article', content)
+        #Create one string from paragraphs
+        content_text = ""
+        comments_text = ""
+
+        for par in content_pars:
+            content_text+=par
+
+        for par in comment_pars:
+            comments_text += par
+
+
+        return [content_text, comments_text]
+
 
 
     def extract_main_container(self, response):
 
-        '''
-        possible upgrades: class*="post"  "post-content", "main-content"
-        '''
+        # 1st rule -- get 1st article element
+        # 2nd rule -- get 1st div whose class attribute value contains "article"
+        # 3rd rule -- get 1st div whose id attribute value contains "post" (for blogs)
+        # backup -- get body element
 
-        main_container = ['no main container']
         if len(response.css('article')) > 0:
             main_container = response.css('article')[0]
+        elif len(response.css('div[class*="article"]')) > 0:
+            main_container = response.css('div[class*="article"]')[0]
         elif len(response.css('div[id*="post"]')) > 0:
             main_container = response.css('div[id*="post"]')[0]
         # elif len(response.css('div[class*="post"]')) > 0:
         #     main_container = response.css('div[id*="post"]')[0]
+        else:
+            main_container = response.css('body')[0]
         return main_container
 
 
+
+    def extract_main_content(self, main_container):
+        content = []
+
+        # Get text form all p inside main container
+        all_p = main_container.xpath('.//p')
+        for p in all_p:
+            if p.css('::text').extract_first() is not None:
+                content.append(p.css('::text').extract_first())
+
+        # Get texts longer than 25 chars form all divs inside main container
+        all_divs_texts = main_container.css('div::text').extract()
+        for div_text in all_divs_texts:
+            if len(div_text) > 25:
+                print(div_text)
+                content.append(div_text)
+        return content
+
+
+
     def extract_comments_inside_main(self, main_container):
+
+        # Get all p inside any element whose id attribute contains 'comment'
         return main_container.css('*[id*="comments"] p::text').extract()
 
 
