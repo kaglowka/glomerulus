cc13770783a736bbd6f3ec5ac0df8875586d9fe0
Kasia <kasi.skowronska@gmail.com>
2018-01-14 13:02:31 +0100

    extracting content paragraphs without comments

--- src/glomerulus/config.py
+++ src/glomerulus/config.py
@@ -1,3 +1,3 @@
 
 # Config data files
-LINKS_PATH = 'links'
+LINKS_PATH = 'links.txt'
--- src/links_spider.py
+++ src/links_spider.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 import scrapy
 
-from glomerulus.config import LINKS_PATH
-from glomerulus.io import FileStorage
+from  glomerulus.config import LINKS_PATH
+from  glomerulus.io import FileStorage
 
 
 class LinksSpider(scrapy.Spider):
@@ -26,4 +26,65 @@ class LinksSpider(scrapy.Spider):
 
     def parse(self, response):
         # Extract structured data and return a dictionary of values to save/use
-        return [{'xyz': 'sdff'}]
+        #print("TUTAJ: " + response.css('title::text').extract_first())
+        #return [{'xyz': 'sdff'}]
+        self.get_paragraphs2(response)
+
+
+    def get_paragraphs2(self, response):
+        article = response.css('article')[0]
+        content = "ARTYKUŁ: "
+
+        all_pars = article.xpath('.//p/text()').extract()
+        comment_pars = article.css('section[id*="comments"] p::text').extract()
+
+        content += "\n TREŚĆ --- \n"
+        for p in all_pars:
+            content += p
+        content += "\n KOMENTARZE --- \n"
+        for p in comment_pars:
+            content += p
+
+        content_pars = []
+        content += "\n KONTENT --- \n"
+        for par in all_pars:
+            for comment_par in comment_pars:
+                is_comment = False
+                if par == comment_par:
+                    is_comment = True
+                    break
+            if is_comment == False:
+                content_pars.append(par)
+                content += par
+
+        FileStorage().save_data('article', content)
+
+    def get_paragraphs(self, response):
+        article = response.css('article')[0]
+        content = "ARTYKUŁ: "
+        for p in article.xpath('.//p/text()').extract():
+            content += p
+        all_pars = []
+        comment_pars = []
+
+        content += "\n TREŚĆ --- \n"
+        for p in article.xpath('.//p/text()').extract():
+            all_pars.append(p)
+            content += p
+        content += "\n KOMENTARZE --- \n"
+        for p in article.css('section[id*="comments"] p::text').extract():
+            comment_pars.append(p)
+            content += p
+        content_pars = []
+        content += "\n KONTENT --- \n"
+        for par in all_pars:
+            for comment_par in comment_pars:
+                is_comment = False
+                if par == comment_par:
+                    is_comment = True
+                    break
+            if is_comment == False:
+                content_pars.append(par)
+                content += par
+
+        FileStorage().save_data('article', content)
