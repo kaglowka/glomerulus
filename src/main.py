fbb52a02b2fe3e213b4473a04997a65b03ab3ff7
KonradZielinski <konrad.zielinski@student.uw.edu.pl>
2018-01-19 13:47:38 +0100

    save to CSV
    Article class added with an ability to save itself to CSV

--- src/main.py
+++ src/main.py
@@ -2,6 +2,6 @@ from glomerulus.io import FileStorage
 from glomerulus.search.search import get_links_for_keywords
 
 # Example search
-get_links_for_keywords('losowe zapytanie')
+get_links_for_keywords('żona krawca mleko kobiece')
 
 
