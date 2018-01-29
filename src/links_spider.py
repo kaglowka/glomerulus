5f9cfb017961919f686cbdb5067d7eebd5217324
Kasia <kasi.skowronska@gmail.com>
2018-01-29 18:00:38 +0100

    content & date extraction code refactored

--- src/links_spider.py
+++ src/links_spider.py
@@ -42,6 +42,7 @@ class LinksSpider(scrapy.Spider):
         self.save_readable_article_data(data)
         return data
 
+
     def save_readable_article_data(self, data):
         # Should be somewhere else? TODO better?
         str = ""
@@ -57,10 +58,10 @@ class LinksSpider(scrapy.Spider):
         # Indentify main container
         article = self.extract_main_container(response)
 
-        # Get all p inside main container
+        # Get all paragraphs inside main container
         all_pars = self.extract_main_content(article)
 
-        # Extract all p likely to be comments inside main container
+        # Extract all paragraphs likely to be comments inside main container
         comment_pars = self.extract_comments_inside_main(article)
 
         # Remove comments from main content
@@ -77,64 +78,53 @@ class LinksSpider(scrapy.Spider):
         for par in comment_pars:
             comments_text += par
 
-
         return [content_text, comments_text]
 
 
 
 
-    def extract_main_container(self, response):
+    # MAIN CONTAINER EXTRACTION
 
-        # 1st rule -- get 1st article element
-        # 2nd rule -- get 1st div whose class attribute value contains "article"
-        # 3rd rule -- get 1st div whose id attribute value contains "post" (for blogs)
+    # the list below defines rules for identifying main container and order in which they should be applied
+
+    candidate_main_container = [
+
+        'article',
+
+        # class contains word 'article'
+        'section[class~="article"]',
+        'div[class~="article"]',
+
+        # id contains word 'article'
+        'section[id~="article"]',
+        'div[id~="article"]',
+
+        # class contains substring 'article'
+        'div[class*="article"]',
+
+        # id contains substring 'article'
+        'div[id*="article"]',
+
+        # identifying posts (for example on blogs)
+        'div[class~="post"]',
+        'div[id~="post"]',
+        'div[class*="post"]',
+        'div[id*="post"]',
+
         # backup -- get body element
+        'body'
+    ]
 
-        candidate = response.css('article')
-        if len(candidate) > 0:
-            return candidate[0]
-
-        candidate = response.css('section[class~="article"]')
-        if len(candidate) > 0:
-            return candidate[0]
-
-        candidate = response.css('div[class~="article"]')
-        if len(candidate) > 0:
-            return candidate[0]
-
-        candidate = response.css('section[id~="article"]')
-        if len(candidate) > 0:
-            return candidate[0]
-
-        candidate = response.css('div[id~="article"]')
-        if len(candidate) > 0:
-            return candidate[0]
-
-        candidate = response.css('div[class*="article"]')
-        if len(candidate) > 0:
-            return candidate[0]
-
-        candidate = response.css('div[class~="post"]')
-        if len(candidate) > 0:
-            return candidate[0]
-
-        candidate = response.css('div[id~="post"]')
-        if len(candidate) > 0:
-            return candidate[0]
-
-        candidate = response.css('div[class*="post"]')
-        if len(candidate) > 0:
-            return candidate[0]
-
-        candidate = response.css('div[id*="post"]')
-        if len(candidate) > 0:
-            return candidate[0]
-
-        candidate = response.css('body')[0]
-
-        return candidate
+    def extract_main_container(self, response):
+        for selector in self.candidate_main_container:
+            candidate = response.css(selector)
+            if len(candidate) > 0:
+                return candidate[0]
 
 
+
+
+    # MAIN CONTENT EXTRACTION
 
     def extract_main_content(self, main_container):
         content = []
@@ -144,17 +134,6 @@ class LinksSpider(scrapy.Spider):
         for p_text in all_p_texts:
             if len(p_text) > 1:
                 content.append(p_text)
-                print("p: " + p_text)
-
-        print("all_p_texts", all_p_texts)
-
-        # all_p_texts_xpath = main_container.xpath('//p//text()').extract()
-        # for p_text in all_p_texts_xpath:
-        #     if len(p_text) > 1:
-        #         content.append(p_text)
-        #         print("p: " + p_text)
-        #
-        # print("all_p_texts_xpath", all_p_texts_xpath)
 
         # Get texts longer than 25 chars form all divs inside main container
         all_divs_texts = main_container.css('div::text').extract()
@@ -162,41 +141,46 @@ class LinksSpider(scrapy.Spider):
             if len(div_text) > 25:
                 div_text = div_text.strip() #remove whitespaces
                 content.append(div_text)
-                print("div: " + div_text)
+
+        # all_p_texts_xpath = main_container.xpath('//p//text()').extract()
+        # for p_text in all_p_texts_xpath:
+        #     if len(p_text) > 1:
+        #         content.append(p_text)
+        #
 
         # all_divs_texts_xpath = main_container.xpath('//div//text()').extract()
         # for div_text in all_divs_texts_xpath:
         #     if len(div_text) > 25:
         #         div_text = div_text.strip() #remove whitespaces
         #         content.append(div_text)
-        #         print("div: " + div_text)
 
-        print("CONTENT", content)
         return content
 
 
 
-    def extract_comments_inside_main(self, main_container):
-        comments = []
-        # Get all p inside any element whose id attribute contains 'comment'
-        all_p_id_comments = main_container.css('*[id*="comment"] p::text').extract()
-        for comment in all_p_id_comments:
-            comment = comment.strip()
-            comments.append(comment)
-        all_p_class_comments = main_container.css('*[class*="comment"] p::text').extract()
-        for comment in all_p_class_comments:
-            comment = comment.strip()
-            comments.append(comment)
+    # COMMENTS EXTRACTION
 
-        all_div_id_comments = main_container.css('*[id*="comment"] div::text').extract()
-        for comment in all_div_id_comments:
-            comment = comment.strip()
-            comments.append(comment)
-        all_div_class_comments = main_container.css('*[class*="comment"] div::text').extract()
-        for comment in all_div_class_comments:
-            comment = comment.strip()
-            comments.append(comment)
+    # the list below defines rules for identifying comments
+
+    candidate_comments = [
+        '*[class*="comment"] p::text',
+        '*[id*="comment"] p::text',
+        '*[class*="comment"] div::text',
+        '*[id*="comment"] div::text',
+    ]
+
+    def extract_comments_inside_main(self, main_container):
+
+        comments = []
+
+        for selector in self.candidate_comments:
+            result = main_container.css(selector).extract()
+            for comment in result:
+                comment = comment.strip()
+                comments.append(comment)
+
         return comments
+
 
 
     def remove_elements(self, all_elements, trash_elements):
@@ -210,7 +194,14 @@ class LinksSpider(scrapy.Spider):
         return return_elements
 
 
+
+
     # DATE EXTRACTION
+
+    candidate_date = [
+        'time::text',
+        '*[class="data"]::text',
+    ]
 
     def extract_publish_date(self, response):
         # date = response.css('time::text').extract_first()
@@ -220,13 +211,9 @@ class LinksSpider(scrapy.Spider):
 
         # TODO: first candidate datetime value
 
-        candidate = response.css('time::text').extract()
-        if len(candidate) > 0:
-            return candidate[0]
-
-        candidate = response.css('*[class="data"]::text').extract()
-        if len(candidate) > 0:
-            return candidate[0]
-
+        for selector in self.candidate_date:
+            candidate = response.css(selector).extract()
+            if len(candidate) > 0:
+                return candidate[0]
 
         return "BRAK DATY"
